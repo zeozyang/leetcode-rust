239508d22b43b16ce129dfaf60a4ee29deb66210
Yang Yan <1337738378@qq.com>
2022-06-30 12:17:57 +0800

    master

--- src/bin/1.两数之和.rs
+++ src/bin/1.两数之和.rs
@@ -9,12 +9,12 @@ use std::collections::HashMap;
 impl Solution {
     pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
         let mut map = HashMap::new();
-        for (i, v) in nums.iter().enumerate() {
-            let other = target - v; // 这里的v是个对i32类型的引用，但被编译器自动解引用了，也可以手动解引用为*v
-            if let Some(&other_i) = map.get(&other) {
-                return vec![i as i32, other_i as i32];
+        for (i, &num) in nums.iter().enumerate() {
+            let other = target - num;
+            if let Some(&j) = map.get(&other) {
+                return vec![i as i32, j as i32];
             }
-            map.insert(v, i);
+            map.insert(num, i);
         }
         vec![]
     }
