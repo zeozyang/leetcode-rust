bbbfc6e17059c0398487ff14e5fc58534c94850d
Yang Yan <1337738378@qq.com>
2022-07-04 17:47:46 +0800

    master

---
+++ src/bin/6.z-字形变换.rs
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode.cn id=6 lang=rust
+ *
+ * [6] Z 字形变换
+ */
+
+// @lc code=start
+impl Solution {
+    pub fn convert(s: String, num_rows: i32) -> String {
+        if num_rows <= 1 {
+            return s;
+        }
+        let mut v = vec!["".to_string(); num_rows as usize]; // 这里用[类型; 长度]的方法初始化数组时，长度要用usize表示
+        let mut row: i32 = 0;
+        let mut flag: i32 = -1; // flag用来表示方向，只有01两个值
+        for a_char in s.chars() {
+            v[row as usize].push(a_char); // 用索引取值时，索引要用uszie表示
+            if row == 0 || row == num_rows - 1 {
+                flag = -flag;
+            }
+            row += flag;
+        }
+
+        let mut res = String::from("");
+        for s in v {
+            res += &s;
+        }
+        res
+    }
+}
+// @lc code=end
